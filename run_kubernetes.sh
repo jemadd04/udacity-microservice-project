ca799f8faf838d00c1ad829fe07c874d54ed75ea
James Maddox <james.maddox07@gmail.com>
2020-04-27 17:13:46 -0400

    pushing to gh

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="jamesmaddox/jmudacity"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run jmproject\
+	--generator=run-pod/v1\
+	--image=$dockerpath\
+	--port=80 --labels app=jmproject
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward jmproject 8000:80
+
